c3223e67be9190c89cb101b4434580f372157a64
boinevishal <53041178+boinevishal@users.noreply.github.com>
2019-07-18 16:14:04 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,18 @@
+#include<studio.h>
+#include<conio.h>
+void main()
+{
+printf("welcome to horizon 2k19);
+int a,b,c,d,e;
+printf("enter value of a:",a)
+scanf("%d",&a);
+printf("Enter value of b:",b);
+scanf("%d",&b);
+c=a+b;
+d=a-b;
+e=a*b
+printf("The addition is:%d",c);
+printf("The subtraction is:%d",d);
+printf("The multiplication is:%d",e);
+getch();
+}
